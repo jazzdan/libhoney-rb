354a915d46cd11cf4bef8c86ef2e23d920d7d445
potatoshima <e.a.nakashima+potato@gmail.com>
2017-06-01 21:05:29 -0700

    WIP: use batch API endpoint

--- lib/libhoney/transmission.rb
+++ lib/libhoney/transmission.rb
@@ -24,7 +24,7 @@ module Libhoney
       @threads = []
       @lock = Mutex.new
     end
-    
+
     def add(event)
       begin
         @send_queue.enq(event, !@block_on_send)
@@ -35,11 +35,12 @@ module Libhoney
       @lock.synchronize {
         return if @threads.length > 0
         while @threads.length < @max_concurrent_batches
-          @threads << Thread.new { self.send_loop }
+          @threads << Thread.new { self.send_batches }
         end
       }
     end
 
+    # TODO:EN: Remove when we are confident that send_batches works
     def send_loop
       # eat events until we run out
       loop {
@@ -66,6 +67,52 @@ module Libhoney
       }
     end
 
+    # TODO:EN: Write tests for this behavior (whoops)
+    def send_batches
+      # eat batches of events until we run out
+      loop {
+        batch = []
+
+        # TODO:EN: This is a hack — need to figure out why pushing nil to the
+        # queue isn't working as a way to break out of the loop
+        break if @send_queue.size < @max_concurrent_batches
+
+        while batch.size < @max_batch_size
+          e = @send_queue.pop
+          break if e.nil?
+          batch.push(e)
+        end
+
+        break if batch.size == 0
+
+        before = Time.now
+        resp = HTTP.headers(
+          'User-Agent' => "libhoney-rb/#{VERSION}",
+          'Content-Type' => 'application/json',
+          'X-Honeycomb-Team' => batch[0].writekey,
+        ).post(
+          URI.join(batch[0].api_host, '/1/batch/', batch[0].dataset), :json => batch.map { |e| batch_event(e) }
+        )
+
+        after = Time.now
+
+        response = Response.new(
+          :duration => after - before,
+          :status_code => resp.status,
+          :metadata => batch[0].metadata
+        )
+
+        # TODO:EN: just for debugging, delete this :)
+        puts resp.status, batch.size
+
+        begin
+          @responses.enq(response, !@block_on_responses)
+        rescue ThreadError
+          # happens if the queue was full and block_on_send = false.
+        end
+      }
+    end
+
     def close(drain)
       # if drain is false, clear the remaining unprocessed events from the queue
       @send_queue.clear if drain == false
@@ -81,6 +128,16 @@ module Libhoney
       @responses.enq(nil)
 
       0
+    end
+
+    private
+
+    # Make the data structure expected by the batch API endpoint
+    def batch_event(e)
+      {
+        time: e.timestamp.iso8601,
+        data: e.data
+      }
     end
   end
 end
